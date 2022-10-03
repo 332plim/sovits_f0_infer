b971e8e3ae6b410da8eda30f502ee6b1a258280c
wild233-sys <58691319+wild233-sys@users.noreply.github.com>
2022-10-03 17:49:31 +0800

    Update recorder.py
    reduce delay of saving

--- recorder.py
+++ recorder.py
@@ -9,6 +9,7 @@ FORMAT = pyaudio.paInt16
 CHANNELS = 1                # 声道数
 RATE = 22050                # 采样率
 CHUNK = 256
+RECORD_SECONDS = 2  # 录音时间
 stream = p.open(format=FORMAT,
                         channels=CHANNELS,
                         rate=RATE,
@@ -16,39 +17,53 @@ stream = p.open(format=FORMAT,
                         frames_per_buffer=CHUNK)
 
 
-def save_audio(WAVE_OUTPUT_FILENAME,CHANNELS,FORMAT,RATE,frames):
+def save_audio(WAVE_OUTPUT_FILENAME,CHANNELS,FORMAT,RATE):
+        global audio_data
         wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
         wf.setnchannels(CHANNELS)
         wf.setsampwidth(p.get_sample_size(FORMAT))
         wf.setframerate(RATE)
-        wf.writeframes(b''.join(frames))
+        wf.writeframes(b''.join(audio_data))
         wf.close()
         
-def get_audio(filepath,stream):
+def get_audio(stream,RECORD_SECONDS):
+        global audio_data
         #a=time.time() 
         CHUNK = 256
-        RECORD_SECONDS = 2  # 录音时间
-        WAVE_OUTPUT_FILENAME = filepath
-        
 
 
 
         #print("*"*10, "开始录音：请在5秒内输入语音")
         frames = []
-        for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+        frames_count=0
+        lenth=int(RATE / CHUNK * RECORD_SECONDS)
+        while TRUE:
             data = stream.read(CHUNK)
             frames.append(data)
-        #print("*"*10, "录音结束\n")
+            frames_count+=1
+            if frames_count == int(RATE / CHUNK * RECORD_SECONDS):
+              audio_data=frames
+              frames=[]
 
-        threading.Thread(target=save_audio, args=(WAVE_OUTPUT_FILENAME,CHANNELS,FORMAT,RATE,frames,)).start()
+
+
+        
         #stream.stop_stream()
         #stream.close()
         #p.terminate()
         #print(time.time()-a)
 
         #print(time.time()-a)
-# 联合上一篇博客代码使用，就注释掉下面，单独使用就不注释
+
+
+
+threading.Thread(target=get_audio, args=(stream,RECORD_SECONDS,)).start()
+print("准备开始录音")
+time.sleep(RECORD_SECONDS)
+last_audio_data=[]
 while True:
     in_path = input_filepath + str(input_filename)+".wav"
-    get_audio(in_path,stream)
-    input_filename+=1
+    if last_audio_data!=audio_data:
+        save_audio(in_path,CHANNELS,FORMAT,RATE)
+        input_filename+=1
+        last_audio_data=audio_data
